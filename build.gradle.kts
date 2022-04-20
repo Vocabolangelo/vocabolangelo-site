b353c4f7e3a22c83f73b4f63a790eaa7ad629f3f
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-04-20 14:36:38 +0200

    Added gradle task and precommit config

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,7 @@
  * This is a general purpose Gradle build.
  * Learn more about Gradle by exploring our samples at https://docs.gradle.org/7.4.1/samples
  */
+
+tasks.register<Exec>("test"){
+    commandLine("turtle", "--validate", "vocabolangelo.ttl")
+}
