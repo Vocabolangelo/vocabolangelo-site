1a3d4b0cd9c6ef7f6231cda88383baa2f9c9ecdb
Angelo Filaseta <filaseta.angelo@gmail.com>
2022-08-16 19:09:18 +0200

    infer task working

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,13 @@ import java.io.ByteArrayOutputStream
 val dataFolder = "_data"
 val originalFileName = "vocabolangelo.ttl"
 val inferredFileName = "vocabolangelo-inferred.xml"
+val mergedFileName = "vocabolangelo-merged.ttl"
 
 fun filePath(fileName: String) = dataFolder + File.separator + fileName
 
 val originalFilePath = filePath(originalFileName)
 val inferredFilePath = filePath(inferredFileName)
+val mergedFilePath = filePath(mergedFileName)
 
 tasks.register<Exec>("test") {
     commandLine("turtle", "--validate", originalFilePath)
@@ -22,11 +24,26 @@ tasks.register<Exec>("test") {
 
 tasks.register("infer") {
     doLast{
-        val outputStream = ByteArrayOutputStream()
-        exec{
-            commandLine("pellet", "extract", originalFilePath)
-            standardOutput = outputStream
+        try {
+            val inferredText: String = ByteArrayOutputStream().use { outputStream ->
+                exec{
+                    commandLine("pellet", "extract", originalFilePath)
+                    standardOutput = outputStream
+                }
+                outputStream.toString()
+            }
+            File(inferredFilePath).writeText(inferredText)
+
+            val mergedText: String = ByteArrayOutputStream().use { outputStream ->
+                exec{
+                    commandLine("riot", "--output=TURTLE", originalFilePath, inferredFilePath)
+                    standardOutput = outputStream
+                }
+                outputStream.toString()
+            }
+            File(mergedFilePath).writeText(mergedText)
+        } finally {
+            File(inferredFilePath).delete()
         }
-        File(inferredFilePath).writeText(outputStream.toString())
     }
 }
