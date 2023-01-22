7d8067c8bd30b01fcedcc2c4c3a3c6a0e62628a4
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-01-22 16:41:21 +0100

    build(inference): riot command used via docker now

--- infer.sh
+++ infer.sh
@@ -3,10 +3,8 @@
 git clone https://github.com/stardog-union/pellet.git build/pellet
 cd build/pellet
 mvn install -pl cli -DskipTest -am
-cd ..
-curl -o https://dlcdn.apache.org/jena/binaries/apache-jena-4.7.0.zip
-unzip -o oapache-jena-4.7.0.zip
-cd ..
+cd ../..
 build/pellet/cli/target/pelletcli/bin/pellet extract _data/vocabolangelo.ttl >> _data/vocabolangelo-inferred.xml
-build/apache-jena-4.7.0/bin/riot --output=TURTLE _data/vocabolangelo.ttl _data/vocabolangelo-inferred.xml >> _data/vocabolangelo-merged.ttl
-rm _data/vocabolangelo-inferred.xml
+docker pull stain/jena
+docker run --volume $PWD/_data:/rdf stain/jena riot --output=TURTLE vocabolangelo.ttl vocabolangelo-inferred.xml > $PWD/_data/vocabolangelo-merged.ttl
+rm  _data/vocabolangelo-inferred.xml
