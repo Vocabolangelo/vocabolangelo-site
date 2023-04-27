d2803bb1e6b645f21fc22c8f6e0e60281c65108a
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-04-27 19:46:24 +0200

    feat(site): add working search bar to vocabolieri page

--- src/routes/Vocabolieri.tsx
+++ src/routes/Vocabolieri.tsx
@@ -3,6 +3,7 @@ import {Person} from '../rdf/types/Person'
 import {vocang} from '../rdf/prefixes'
 import DefaultLayout from '../components/common/DefaultLayout'
 import {AlphabeticList} from '../components/common/AlphabeticList'
+import SearchBar from '../components/common/SearchBar'
 
 export const VOCABOLIERI_ROUTE = '/vocabolieri'
 
@@ -10,6 +11,8 @@ export default function Vocabolieri(){
 
     const noPeople: Person[] = []
     const [people, setPeople] = useState(noPeople)
+    const [searchValue, setSearchValue]= useState<string>('')
+
     useEffect(() => {
         Person.all().then(people =>
             setPeople(people)
@@ -20,17 +23,28 @@ export default function Vocabolieri(){
         return person.lastName.charAt(0).toLowerCase() === letter
     }
 
+    function searchFilterStrategy(person: Person, str: string): boolean {
+        return person.fullName().toLowerCase().includes(str)
+    }
+
     return <DefaultLayout
         title={'Vocabolieri'}
-        subtitle={null}
-        content={
+        subtitle={
+            'Ogni Vocaboliere ha inventato almeno una Parolangelo in tutto l\'arco della sua vita.' +
+            'Non è ancora stato dimostrato che inventare parolangelo allunghi la vita ma ci stiamo lavorando.'
+        }
+        content={<>
+            <SearchBar onSearch={(search) => setSearchValue(search)}/>
             <AlphabeticList
                 list={people}
                 elementKey={person => person.relativeUri(vocang)}
                 elementContent={person => <p>{person.lastName} {person.firstName}</p>}
                 elementLink={person => '/vocabolieri/' + person.relativeUri(vocang)}
+                searchString={searchValue}
+                searchFilterStrategy={searchFilterStrategy}
                 alphabeticStrategy={alphabeticStrategy}
             />
+        </>
         }
     />
 }
