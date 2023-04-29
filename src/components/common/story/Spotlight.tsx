fdf082062403985ac967e37789e4af5a9ff06720
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-04-29 11:06:11 +0200

    feat(site): add Spotlight component

---
+++ src/components/common/story/Spotlight.tsx
@@ -0,0 +1,15 @@
+/**
+ * Spotlight element from the Story Layout.
+ * @param {SpotlightProps} props the component props.
+ */
+export default function Spotlight(props: SpotlightProps) {
+    const {style, optionalModifiers, content, imageUrl, imageAlt} = props
+    return <section className={`spotlight style${style} ${optionalModifiers?.map(m => m)}`}>
+        <div className="content">
+            {content}
+        </div>
+        <div className="image">
+            <img src={imageUrl} alt={imageAlt}/>
+        </div>
+    </section>
+}
---
+++ src/components/props/SpotlightProps.tsx
@@ -0,0 +1,10 @@
+/**
+ * Props of a Spotlight
+ */
+interface SpotlightProps {
+    style: number
+    optionalModifiers?: string[]
+    content: JSX.Element
+    imageUrl: string
+    imageAlt: string
+}
