099f3a37ffee968f83f29f683316132ef152fabb
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-04-29 11:33:21 +0200

    refactor(site): migrate page 404 to SPotlight component

--- src/Page404.tsx
+++ src/Page404.tsx
@@ -2,26 +2,26 @@ import {Link} from 'react-router-dom'
 import {PAROLANGELO_ROUTE} from './routes/Parolangelo'
 import Header from './components/common/Header'
 import Footer from './components/common/Footer'
+import Spotlight from './components/common/story/Spotlight'
 
 export function Page404() {
     return <>
         <Header/>
-        <section className="spotlight style1 orient-left content-align-left image-position-center onscroll-image-fade-in">
-            <div className="content">
-                <h1>404</h1>
-                <h3>A tutti capita di prendere <Link to={`${PAROLANGELO_ROUTE}/scorciatroia`}>scorciatroie</Link> nella vita.</h3>
-                <p>
-                </p>
-                <div className="actions stacked">
-                    <a href={'/'} className="button big wide smooth-scroll-middle">
-                            Home
-                    </a>
-                </div>
-            </div>
-            <div className="image">
-                <img src="images/map.jpg" alt="Mappa e bussola"/>
-            </div>
-        </section>
+        <Spotlight
+            style={1}
+            optionalModifiers={['orient-left', 'content-align-left', 'image-position-center', 'onscroll-image-fade-in']}
+            content={
+                <>
+                    <h1>404</h1>
+                    <h3>A tutti capita di prendere <Link to={`${PAROLANGELO_ROUTE}/scorciatroia`}>scorciatroie</Link> nella vita.</h3>
+                    <div className="actions stacked">
+                        <a href={'/'} className="button big wide smooth-scroll-middle">Home</a>
+                    </div>
+                </>
+            }
+            imageUrl={'images/map.jpg'}
+            imageAlt={'Mappa e bussola'}
+        />
         <Footer/>
     </>
 }
