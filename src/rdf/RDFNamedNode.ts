27f0e9f2ec1887258da6a72e97271ccd7196ae92
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-05-04 21:23:20 +0200

    fix(site): remove unused imports

--- src/rdf/RDFNamedNode.ts
+++ src/rdf/RDFNamedNode.ts
@@ -4,8 +4,6 @@ import {Quad_Object} from 'rdflib/lib/tf-types'
 import {RDFStore} from './RDFStore'
 import {rdf} from './prefixes'
 import {Prefix} from './Prefix'
-import {Person} from './types/Person'
-import {Concept} from './types/Concept'
 
 /**
  * A class representing an RDF Node. It can be extended by other classes and be enriched.
