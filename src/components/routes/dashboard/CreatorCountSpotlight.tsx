59244ea8e9c80a5e3ad1ee940ce1edf7f5cae89a
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-05-04 20:01:02 +0200

    feat(site): add gender statistic to dashboard

--- src/components/routes/dashboard/CreatorCountSpotlight.tsx
+++ src/components/routes/dashboard/CreatorCountSpotlight.tsx
@@ -8,11 +8,15 @@ import Spotlight from '../../common/story/Spotlight'
  * Spotlight that present the number of Vocabolieri in the Vocabolangelo Project.
  */
 export default function CreatorCountSpotlight() {
-    const [creatorCount, setCreatorCount] = useState(0)
+    const [maleCount, setMaleCount] = useState(0)
+    const [femaleCount, setFemaleCount] = useState(0)
+    const [otherCount, setOtherCount] = useState(0)
 
     useEffect(() => {
         Person.all().then(creators => {
-            setCreatorCount(creators.length)
+            setMaleCount(creators.filter(c => c.gender === 'male').length)
+            setFemaleCount(creators.filter(c => c.gender === 'female').length)
+            setOtherCount(creators.filter(c => c.gender !== 'female' && c.gender !== 'male').length)
         })
     }, [])
 
@@ -22,10 +26,14 @@ export default function CreatorCountSpotlight() {
         imageUrl={'/images/people.jpg'}
         imageAlt={'Collaborazione'}
     >
-        <h1> {creatorCount} </h1>
-        <p>
+        <h1> {maleCount + femaleCount + otherCount} </h1>
+        <p className={'major'}>
             Sono i <Link to={`${PAROLANGELO_ROUTE}/vocaboliere`}>vocabolieri</Link> del
             <strong> Vocabolangelo</strong>. <br/>
+            I maschi sono <strong>{maleCount}</strong>, le femmine sono <strong>{femaleCount}</strong>,
+            i restanti (<strong>{otherCount}</strong>) non si identificano nel binarismo di genere.
+        </p>
+        <p>
             Grazie all&apos; <Link to={`${PAROLANGELO_ROUTE}/apostolangelo`}>
                 apostolangelo
             </Link> un giorno questo numero sarà molto più alto.
