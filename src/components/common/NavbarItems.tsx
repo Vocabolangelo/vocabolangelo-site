fb61f8efdec78f50b507dd0a0a23f25a871661ad
Angelo Filaseta <filaseta.angelo@gmail.com>
2023-08-01 17:55:33 +0200

    feat(site): re-add vocabolieri to navbar and sidebar

--- src/components/common/NavbarItems.tsx
+++ src/components/common/NavbarItems.tsx
@@ -1,21 +1,29 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faFont, faQuoteLeft, faPhotoFilm} from '@fortawesome/free-solid-svg-icons'
+import {faFont, faQuoteLeft, faPhotoFilm, faPerson} from '@fortawesome/free-solid-svg-icons'
+import {PAROLANGELO_ROUTE, SLANGELO_ROUTE} from '../routes/concept/ConceptList'
+import {CONTENUTI_ROUTE} from '../routes/contenuti/ContenutiIndex'
+import {VOCABOLIERI_ROUTE} from '../routes/vocabolieri/Vocabolieri'
 
 const navbarItems = [
     {
         icon: <FontAwesomeIcon icon={faFont} color={'white'}/>,
         title: 'Parolangelo',
-        link: '/parolangelo',
+        link: PAROLANGELO_ROUTE,
     },
     {
         icon: <FontAwesomeIcon icon={faQuoteLeft} color={'white'}/>,
         title: 'Slangelo',
-        link: '/slangelo',
+        link: SLANGELO_ROUTE,
+    },
+    {
+        icon: <FontAwesomeIcon icon={faPerson} color={'white'}/>,
+        title: 'Vocabolieri',
+        link: VOCABOLIERI_ROUTE,
     },
     {
         icon: <FontAwesomeIcon icon={faPhotoFilm} color={'white'}/>,
         title: 'Contenuti',
-        link: '/contenuti',
+        link: CONTENUTI_ROUTE,
     },
 ]
 
